48d97e93880a75b3b8ba89bcc4cf5e2f6207d764
kzkedzierska <kzkedzierska@gmail.com>
2019-09-18 09:34:14 +0100

    Added part 6

---
+++ Part6/install_script.R
@@ -0,0 +1,27 @@
+#!/bin/Rscript
+required_packages <- c("tidyverse", "ggsci")
+required_biopackages <- c("airway", "DESeq2", "Rsamtools", "GenomicFeatures",
+                          "GenomicAlignments","BiocParallel")
+
+tmp <- 
+  lapply(required_packages, function(pkg) {
+    if(!require(pkg, quietly = TRUE, 
+                character.only = TRUE)) {
+      print(paste0("WARNiNG! Installlig package ", 
+                   pkg))
+      install.packages(pkg)
+    }
+  })
+
+bio_tmp <- 
+  lapply(required_biopackages, function(pkg) {
+    if(!require(pkg, quietly = TRUE, 
+                character.only = TRUE)) {
+      if (!requireNamespace("BiocManager", quietly = TRUE)) {
+        print(paste0("WARNiNG! Installlig package ", 
+                     pkg))
+        install.packages("BiocManager")
+      }
+      BiocManager::install(pkg)
+    }
+  })
